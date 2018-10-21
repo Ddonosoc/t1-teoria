2cf498ea77199b953418d3135e11fa9cca7f5897
Diego <diegomrbear@hotmail.com>
2018-10-21 13:00:51 -0300

    creacion archivo para afd

---
+++ AFND2AFD.py
@@ -0,0 +1,13 @@
+
+
+# Funcion que convierte de AFND a AFD usando el algoritmo visto en clases
+def AFND2AFD(automata):
+
+    return 0
+
+
+# Funcion que busca algun camino desde cierto estado usando como simbolo solo epsilon o el simbolo dado
+# Retorna un arreglo de estados que cumplen lo anterior
+def Path_search(automata, estadoI, simbolo):
+
+    return 0
