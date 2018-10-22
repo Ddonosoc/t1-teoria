dad3570304f0fdb94a4508d48f25c84652ccb047
Diego <diegomrbear@hotmail.com>
2018-10-22 18:06:45 -0300

    inverso automata, tiene problemas

--- AFND2AFD.py
+++ AFND2AFD.py
@@ -128,29 +128,3 @@ def find(automata, estado):
             result = elem
             break
     return result
-
-
-e = []
-for i in range(0, 11):
-    e.append(estado(str(i)))
-
-e[0].trans.append([e[1], ''])
-e[0].trans.append([e[10], ''])
-e[1].trans.append([e[2], ''])
-e[1].trans.append([e[5], ''])
-e[2].trans.append([e[3], 'a'])
-e[3].trans.append([e[4], 'b'])
-e[4].trans.append([e[9], ''])
-e[9].trans.append([e[1], ''])
-e[5].trans.append([e[6], 'a'])
-e[6].trans.append([e[7], 'b'])
-e[7].trans.append([e[8], 'a'])
-e[8].trans.append([e[9], ''])
-e[9].trans.append([e[10], ''])
-
-e[10].final = 1
-
-afd = AFND2AFD(e, 'ab')
-for elem in afd:
-    print(elem.id, elem.final)
-
